5790a53686f0ec050ca8d8c6ff7a8c9a393c4087
eduardonk9999 <thiago.novak7@gmail.com>
2022-04-07 19:58:04 -0300

    tipo de dados

---
+++ tipodedados.js
@@ -0,0 +1,64 @@
+/**
+ * Strings Tipo de Caracteres
+ * 
+*/
+
+console.log("Eduardo")
+
+
+/**
+ * Numbers
+ * 33 -> Inteiro
+ * 12.5 -> reais - float
+ * NaN -> Not a Number
+ * 
+*/
+
+console.log(33)
+console.log(13 + 5)
+console.log(13 / 5)
+
+/**
+ * Boolean
+ * true
+ * false
+ * 
+*/
+
+console.log(true)
+console.log(false)
+
+
+/**
+ * null => nulo
+ * objeto que não possui nada dentro
+ * diferente de indefinido (undefined)
+ * 
+*/
+console.log(null)
+
+/**
+ * Object
+ * Objeto
+ * Propriedades / Atributos
+ * Funcionalidade / Métodos
+ * propriedade: valor
+*/
+
+console.log({
+  name: "Edu",
+  age: 30,
+  andar: function() {
+    console.log('andar')
+  }
+})
+
+/**
+ * Array (Vetores)
+ * - Uma Lista
+ * - Agrupamento de dados
+ * ["Eduardo", 30]
+ * 
+*/
+console.log(["Eduardo", 30])
+
