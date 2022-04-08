1afef15556a0475299c502e8ee2fc01d2c99df4c
eduardonk9999 <thiago.novak7@gmail.com>
2022-04-08 09:58:22 -0300

    variaves, escopo

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+/**
+ * Espaços na memória para guardar informações
+*/
+
+var name = "Eduardo";
+
+//Escopo partes do bloco em que colocamos código
+// Escopo global
+const variavelEscopoGlobal = 'Sou escopo Global'
+function escopoLocal() {
+  const variavelEscopoLocal = 'Sou escopo Local'
+}
+// const e les são locais
+
+
+
+
