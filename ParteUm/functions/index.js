0968631beab9fb8464e744e5a8c6e01b68bf952d
eduardonk9999 <thiago.novak7@gmail.com>
2022-04-10 09:36:12 -0300

    functions parte 1

---
+++ ParteUm/functions/index.js
@@ -0,0 +1,23 @@
+/**
+ * Functions
+ * 
+*/
+
+function createPhrases() {
+  console.log('Estudar é muito bom!!!')
+}
+
+createPhrases();
+
+
+/**
+ * Argumentos e Parametros
+ * 
+ * parametros
+*/
+
+// Function expression || function anonymous
+const sum = function(number1, number2) {
+  return number1 + number2 
+}
+sum(2, 3) // 5
