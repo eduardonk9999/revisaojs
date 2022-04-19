ee35e8fcafe4b5ad8c8570c35ec48dfa0b8ed50c
eduardonk9999 <thiago.novak7@gmail.com>
2022-04-19 09:23:24 -0300

    Add, revisao operadores

--- ParteUm/expressoeseoperadores/index.js
+++ ParteUm/expressoeseoperadores/index.js
@@ -3,3 +3,25 @@
 */
 let name = new String('Eduardo')
 let age = new Number(23)
+
+
+// Grouping operator => ()
+let total = (2 + 3) + 5
+
+// Operadores de comparação
+
+// == igual a 
+let one = 1
+let two = 2
+console.log(one + two)
+
+// diferente de !=
+console.log(one != two)
+
+// === estritamente igual, ele compara o valor e o tipo
+console.log(one === two)
+
+// & => True, só se as duas opções forem verdadeiras
+// || => True, se um for verdadeiro, false se os dois forem falses
+// ! NOT => !true => false
+
