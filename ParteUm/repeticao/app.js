b35283fc87cf8250dfb9b743e3302d894e5b4d21
eduardonk9999 <thiago.novak7@gmail.com>
2022-04-21 10:41:42 -0300

    estrutura de repeticoes

---
+++ ParteUm/repeticao/app.js
@@ -0,0 +1,23 @@
+//for 
+for(let i = 0; i < 10; i++) {
+  console.log(i)
+}
+
+//whilw
+let i = 0
+while(i < 10) {
+  console.log(i)
+
+  i++
+}
+
+// for...of
+
+let name = 'Eduardo'
+let names = ['João', 'Paulo', 'Pedro']
+
+for(let name of names) {
+  console.log(name)
+}
+
+
