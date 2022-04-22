c1aa157fa360baeb6a8e41a39803f545f7abe49a
eduardonk9999 <thiago.novak7@gmail.com>
2022-04-22 09:15:33 -0300

    exercricios

---
+++ ParteUm/Desafios/notasescolares.js
@@ -0,0 +1,17 @@
+/**
+ * Crie um algoritimo que transforme as notas do sistema
+ * numérico para sistema de notas em caracteres tipo A B C...
+ * de 90 pra cima  A
+ * entre 80 - 90 B
+ * 70 - 79 C
+ * 60 - 69 D
+ * menor que 60 - F
+*/
+
+function notaAluno(nota) {
+ 
+  
+  console.log(nota)
+}
+
+notaAluno(80)
