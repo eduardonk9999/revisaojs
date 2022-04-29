4c051f31ee4c7784ba2358a95b640bde201e06f5
eduardonk9999 <thiago.novak7@gmail.com>
2022-04-29 12:23:01 -0300

    assincrono

---
+++ Assincrino/app.js
@@ -0,0 +1,11 @@
+/**
+ * Sistema síncrono (synchronous), uma tarefa é
+ * concluida após a outra.
+ *
+*/
+
+/**
+ * Assíncrono
+ * Em um sistema assíncrono, tarefas sçai executadas
+ * de maneira independente uma da outra.
+*/
